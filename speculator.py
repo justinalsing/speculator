a83282fff1a6d1fa5c781fed577c61f012ca7137
Justin Alsing <j.a.alsing@gmail.com>
2020-03-24 13:35:04 +0100

    first push of speculator code

---
+++ speculator.py
@@ -0,0 +1,342 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tqdm.auto import tqdm
+import pickle
+dtype = tf.float32
+
+class Speculator(tf.keras.Model):
+    """
+    SPECULATOR network
+    """
+
+    def __init__(self, n_parameters=None, wavelengths=None, pca_transform_matrix=None, parameters_shift=None, parameters_scale=None, pca_shift=None, pca_scale=None, spectrum_shift=None, spectrum_shift=None, n_hidden=[50,50], restore=False, restore_filename=None):
+        
+        """
+        Constructor.
+        :param n_parameters: number of SED model parameters (inputs to the network)
+        :param n_wavelengths: number of wavelengths in the modelled SEDs
+        :param n_pcas: number of PCA components
+        :param pca_transform: the PCA basis vectors, ie., an [n_pcas x n_wavelengths] matrix
+        :param parameters_shift_scale: [parameters_shift, parameters_scale] shift and scale (vectors) for input parameters
+        :param pca_shift_scale: [pca_shift, pca_scale] shift and scale (vectors) for PCA coefficients
+        :param spectrum_shift_scale: [spectrum_shift, spectrum_scale] shift and scale (vectors) for the output spectra
+        :param n_hiddens: list with number of hidden units for each hidden layer
+        """
+        
+        # super
+        super(Speculator, self).__init__()
+        
+        # restore
+        if restore is True:
+            self.restore_attributes(restore_filename)
+            
+        # else set variables from input parameters
+        else:
+            # parameters
+            self.n_parameters = n_parameters
+            self.n_wavelengths = pca_transform_matrix.shape[-1]
+            self.n_pcas = pca_transform_matrix.shape[0]
+            self.n_hidden = n_hidden
+            self.wavelengths = wavelengths
+
+            # architecture
+            self.architecture = [self.n_parameters] + self.n_hidden + [self.n_pcas]
+            self.n_layers = len(self.architecture) - 1
+
+            # PCA transform matrix
+            self.pca_transform_matrix_ = pca_transform_matrix
+
+            # shifts and scales...
+        
+            # input parameters shift and scale
+            self.parameters_shift_ = parameters_shift if parameters_shift is not None else np.zeros(self.n_parameters)
+            self.parameters_scale_ = parameters_scale if parameters_scale is not None else np.ones(self.n_parameters)
+
+            # PCA shift and scale
+            self.pca_shift_ = pca_shift if pca_shift is not None else np.zeros(self.n_pcas)
+            self.pca_scale_ = pca_scale if pca_scale is not None else np.ones(self.n_pcas)
+
+            # spectrum shift and scale
+            self.spectrum_shift_ = spectrum_shift if spectrum_shift is not None else np.zeros(self.n_wavelengths)
+            self.spectrum_scale_ = spectrum_scale if spectrum_scale is not None else np.ones(self.n_wavelengths)
+
+        # shifts and scales and transform matrix into tensorflow constants...
+        
+        # input parameters shift and scale
+        self.parameters_shift = tf.constant(self.parameters_shift_, dtype=dtype, name='parameters_shift')
+        self.parameters_scale = tf.constant(self.parameters_scale_, dtype=dtype, name='parameters_scale')
+        
+        # PCA shift and scale
+        self.pca_shift = tf.constant(self.pca_shift_, dtype=dtype, name='pca_shift')
+        self.pca_scale = tf.constant(self.pca_scale_, dtype=dtype, name='pca_scale')
+        
+        # spectrum shift and scale
+        self.spectrum_shift = tf.constant(self.spectrum_shift_, dtype=dtype, name='spectrum_shift')
+        self.spectrum_scale = tf.constant(self.spectrum_scale_, dtype=dtype, name='spectrum_scale')
+        
+        # pca transform matrix
+        self.pca_transform_matrix = tf.constant(self.pca_transform_matrix_, dtype=dtype, name='pca_transform_matrix')
+        
+        # trainable variables...
+        
+        # weights, biases and activation function parameters for each layer of the network
+        self.W = []
+        self.b = []
+        self.alphas = []
+        self.betas = [] 
+        for i in range(self.n_layers):
+            self.W.append(tf.Variable(tf.random.normal([self.architecture[i], self.architecture[i+1]], 0., np.sqrt(2./self.n_parameters)), name="W_" + srt(i)))
+            self.b.append(tf.Variable(tf.zeros([self.architecture[i+1]]), name = "b_" + str(i)))
+            self.alphas.append(tf.Variable(tf.random.normal([self.architecture[i+1]]), name = "alphas_" + str(i)))
+            self.betas.append(tf.Variable(tf.random.normal([self.architecture[i+1]]), name = "betas_" + str(i)))
+        
+        # restore weights if restore = True
+        if restore is True:
+            self.restore_weights(restore_filename)
+            
+    # non-linear activation function
+    def activation(self, x, alpha, beta):
+        
+        return tf.multiply(tf.add(beta, tf.multiply(tf.sigmoid(tf.multiply(alpha, x)), tf.subtract(1.0, beta)) ), x)
+
+    # call: forward pass through the network to predict pca coefficients
+    def call(self, parameters):
+        
+        outputs = []
+        layers = [tf.divide(tf.subtract(parameters, self.parameters_shift), self.parameters_scale)]
+        for i in range(self.n_layers):
+            
+            # linear network operation
+            outputs.append(tf.add(tf.matmul(layers[-1], self.W[i]), self.b[i]))
+            
+            # non-linear activation function
+            layers.append(self.activation(outputs[-1], self.alphas[i], self.betas[i]))
+            
+        # rescale the output (predicted PCA coefficients) and return
+        return tf.add(tf.multiple(layers[-1], self.pca_scale), self.pca_shift)
+            
+    # pass inputs through the network to predict spectrum
+    def spectrum(self, parameters):
+        
+        # pass through network to compute PCA coefficients
+        pca_coefficients = self.call(parameters)
+        
+        # transform from PCA to normalized spectrum basis; shift and re-scale normalized spectrum -> spectrum
+        return tf.add(tf.multiply(tf.matmul(pca_coefficients, self.pca_transform_matrix), self.spectrum_scale), self.spectrum_shift)
+    
+    # save network parameters to numpy arrays
+    def update_emulator_parameters(self):
+        
+        # put network parameters to numpy arrays
+        self.W_ = [self.W[i].numpy() for i in range(self.n_layers)]
+        self.b_ = [self.b[i].numpy() for i in range(self.n_layers)]
+        self.alphas_ = [self.alphas[i].numpy() for i in range(self.n_layers)]
+        self.betas_ = [self.betas[i].numpy() for i in range(self.n_layers)]
+        
+        # put shift and scale parameters to numpy arrays
+        self.parameters_shift_ = self.parameters_shift.numpy()
+        self.parameters_scale_ = self.parameters_scale.numpy()
+        self.pca_shift_ = self.pca_shift.numpy()
+        self.pca_scale_ = self.pca_scale.numpy()
+        self.spectrum_shift_ = self.spectrum_shift.numpy()
+        self.spectrum_scale_ = self.spectrum_scale.numpy()
+        
+        # pca transform matrix
+        self.pca_transform_matrix_ = self.pca_transform_matrix.numpy()
+        
+    # save
+    def save(self, filename):
+        
+        # serialize weights to HDF5
+        self.save_weights(filename + "_weights.h5")
+        
+        # attributes
+        attributes = [self.W_, 
+                      self.b_, 
+                      self.alphas_, 
+                      self.betas_, 
+                      self.parameters_shift_, 
+                      self.parameters_scale_,
+                      self.pca_shift_,
+                      self.pca_scale_,
+                      self.spectrum_shift_,
+                      self.spectrum_scale_,
+                      self.pca_transform_matrix_,
+                      self.n_parameters,
+                      self.n_wavelengths,
+                      self.wavelengths,
+                      self.n_pcas,
+                      self.n_hidden,
+                      self.n_layers,
+                      self.architecture]
+        
+        # save attributes to file
+        f = open(filename + "_attributes.pkl", 'wb')
+        pickle.dump(attributed, f)
+        f.close()
+        
+    # restore attributes
+    def restore_attributes(self, filename):
+        
+        # load all other attributes
+        f = open(filename + "_attributes.pkl", 'rb')
+        self.W_, self.b_, self.alphas_, self.betas_, self.parameters_shift_, \
+        self.parameters_scale_, self.pca_shift_, self.pca_scale_, self.spectrum_shift_, \
+        self.spectrum_scale_, self.pca_transform_matrix_, self.n_parameters, self.n_wavelengths, \
+        self.wavelengths, self.n_pcas, self.n_hidden, self.n_layers, self.architecture = pickle.load(f)
+        f.close()
+        
+    # restore weights
+    def restore_weights(self, filename):
+        
+        # load weights
+        self.load_weights(filename + ".h5")
+    
+    # forward prediction of spectrum given input parameters
+    def emulate(self, parameters):
+        
+        # forward pass through the network
+        act = []
+        layers = [parameters]
+        for i in range(self.n_layers-1):
+
+            # linear network operation
+            act.append(np.dot(layers[-1], self.W_[i]) + self.b_[i])
+
+            # pass through activation function
+            layers.append((self.betas_[i] + (1.-self.betas_[i])*1./(1.+np.exp(-self.alphas_[i]*act[-1])))*act[-1])
+
+        # final (linear) layer -> (normalized) PCA coefficients
+        layers.append(np.dot(layers[-1], self.W_[-1]) + self.b_[-1])
+
+        # rescale PCA coefficients, multiply out PCA basis -> normalized spectrum, shift and re-scale spectrum -> output spectrum
+        return np.dot(layers[-1]*self.pca_scale_ + self.pca_shift_, self.pca_transform_matrix_)*self.spectrum_scale_ + self.spectrum_shift_
+
+
+
+
+class SpectrumPCA():
+    """
+    SPECULATOR PCA compression class
+    """
+    
+    def __init__(self, n_parameters, n_wavelengths, n_pcas, spectrum_filenames, parameter_filenames, parameter_selection = None):
+        """
+        Constructor.
+        :param n_parameters: number of SED model parameters (inputs to the network)
+        :param n_wavelengths: number of wavelengths in the modelled SEDs
+        :param n_pcas: number of PCA components
+        :param spectrum_filenames: list of .npy filenames for spectra (each one an [n_samples, n_wavelengths] array)
+        :param parameter_filenames: list of .npy filenames for parameters (each one an [n_samples, n_parameters] array)
+        """
+        
+        # input parameters
+        self.n_parameters = n_parameters
+        self.n_wavelengths = n_wavelengths
+        self.n_pcas = n_pcas
+        self.spectrum_filenames = spectrum_filenames
+        self.parameter_filenames = parameter_filenames
+        self.n_batches = len(self.parameter_filenames)
+
+        # PCA object
+        self.PCA = IncrementalPCA(n_components=self.n_pcas)
+        
+        # parameter selection (implementing any cuts on strange parts of parameter space)
+        self.parameter_selection = parameter_selection
+
+    # compute shift and scale for spectra and parameters
+    def compute_spectrum_parameters_shift_and_scale(self):
+        
+        # shift and scale
+        self.spectrum_shift = np.zeros(n_wavelengths)
+        self.spectrum_scale = np.zeros(n_wavelengths)
+        self.parameter_shift = np.zeros(n_parameters)
+        self.parameter_scale = np.zeros(n_parameters)
+        
+        # loop over training data files, accumulate means and std deviations
+        for i in range(self.n_batches):
+            
+            # accumulate assuming no parameter selection
+            if self.parameter_selection is None:
+                self.spectrum_shift += np.mean(np.load(self.spectrum_filenames[i]), axis=0)/self.n_batches
+                self.spectrum_scale += np.std(np.load(self.spectrum_filenames[i]), axis=0)/self.n_batches
+                self.parameter_shift += np.mean(np.load(self.parameter_filenames[i]), axis=0)/self.n_batches
+                self.parameter_scale += np.std(np.load(self.parameter_filenames[i]), axis=0)/self.n_batches
+            # else make selections and accumulate
+            else:
+                # import spectra and make parameter-based cut
+                spectra = np.load(self.spectrum_filenames[i])
+                parameters = np.load(self.parameter_filenames[i])
+                selection = self.parameter_selection(parameters)
+                
+                # update shifts and scales
+                self.spectrum_shift += np.mean(spectra[selection,:], axis=0)/self.n_batches
+                self.spectrum_scale += np.std(spectra[selection,:], axis=0)/self.n_batches
+                self.parameter_shift += np.mean(parameters[selection,:], axis=0)/self.n_batches
+                self.parameter_scale += np.std(parameters[selection,:], axis=0)/self.n_batches             
+                
+    # train PCA incrementally
+    def train_pca(self):
+        
+        # loop over training data files, increment PCA
+        for i in range(self.n_batches):
+            
+            if self.parameter_selection is None:
+            
+                # load spectra and shift+scale
+                normalized_spectra = (np.load(self.spectrum_filenames[i]) - self.spectrum_shift)/self.spectrum_scale
+
+                # partial PCA fit
+                self.PCA.partial_fit(normalized_spectra)
+                
+            else:
+                
+                # select based on parameters
+                selection = self.parameter_selection(np.load(self.parameter_filenames[i]))
+                
+                # load spectra and shift+scale
+                normalized_spectra = (np.load(self.spectrum_filenames[i])[selection,:] - self.spectrum_shift)/self.spectrum_scale
+
+                # partial PCA fit
+                self.PCA.partial_fit(normalized_spectra)
+            
+        # set the PCA transform matrix
+        self.pca_transform_matrix = self.PCA.components_
+        
+    # transform the training data set to PCA basis
+    def transform_and_stack_training_data(self, filename, retain = False):
+        
+        # transform the spectra to PCA basis
+        training_pca = np.concatenate([self.PCA.transform((np.load(self.spectrum_filenames[i]) - self.spectrum_shift)/self.spectrum_scale) for i in range(self.n_batches)])
+        
+        # stack the input parameters
+        training_parameters = np.concatenate([np.load(self.parameter_filenames[i]) for i in range(self.n_batches)])
+        
+        if self.parameter_selection is not None:
+            selection = self.parameter_selection(training_parameters)
+            training_pca = training_pca[selection,:]
+            training_parameters = training_parameters[selection,:]
+            
+        # shift and scale of PCA basis
+        self.pca_shift = np.mean(self.training_pca, axis=0)
+        self.pca_scale = np.std(self.training_pca, axis=0)
+        
+        # save stacked transformed training data
+        np.save(filename + '_pca.npy', training_pca)
+        np.save(parameter_filename + '_parameters.npy', training_parameters)
+        
+        # retain training data as attributes if retain == True
+        if retain:
+            self.training_pca = training_pca
+            self.training_parameters = training_parameters
+            
+    # make a validation plot of the PCA given some validation data
+    def validate_pca_basis(self, parameters_filename, spectrum_filename):
+        
+        # load in the data (and select based on parameter selection if neccessary)
+        
+        # transform to PCA basis and back
+        
+        # make the plot
+
