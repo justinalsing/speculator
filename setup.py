0702ab73be2c6d04fd588f2354399ed60918fb18
Justin Alsing <j.a.alsing@gmail.com>
2020-11-16 11:52:50 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import sys
+
+setup(name='speculator',
+      version='v0.1',
+      description='SPS emulation',
+      author='Justin Alsing',
+      url='https://github.com/justinalsing/speculator',
+      packages=find_packages(),
+      install_requires=[
+          "tqdm>=4.41.1",
+          "tensorflow>=2.3.0",
+          "sklearn"
+      ])
