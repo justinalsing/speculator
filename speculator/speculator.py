8c87f791a380fbc6ebcf35bc43059ec9aa32cd73
Justin Alsing <justinalsing@Justins-MacBook-Pro.local>
2021-06-11 13:00:47 +0200

    added photulator training stack infrastructure

--- speculator/speculator.py
+++ speculator/speculator.py
@@ -1541,3 +1541,78 @@ class PhotulatorModelStack:
     def magnitudes(self, theta, N):
 
         return tf.concat([tf.add(self.emulators[i](theta), tf.expand_dims(N, -1)) for i in range(self.n_emulators)], axis=-1)
+
+# train photulator model stack
+def train_photulator_stack(training_theta, training_mags, parameters_shift, parameters_scale, magnitudes_shift, magnitudes_scale, n_layers=4, n_units=128, filters=None, validation_split=0.1, lr=[1e-3, 1e-4, 1e-5, 1e-6], batch_size=[1000, 10000, 50000, 1000000], gradient_accumulation_steps=[1, 1, 1, 10], epochs=1000, patience=20, root_dir='', verbose=True)
+
+    # architecture
+    n_hidden = [n_units]*n_layers
+    
+    # train each band in turn
+    for f in range(len(filters)):
+
+        if verbose is True:
+            print('filter ' + filters[f] + '...')
+
+        # construct the PHOTULATOR model
+        photulator = Photulator(n_parameters=training_theta.shape[-1], 
+                           filters=[filters[f]], 
+                           parameters_shift=parameters_shift, 
+                           parameters_scale=parameters_scale, 
+                           magnitudes_shift=magnitudes_shift[f], 
+                           magnitudes_scale=magnitudes_scale[f], 
+                           n_hidden=[128, 128, 128, 128], 
+                           restore=False, 
+                           restore_filename=None,
+                           optimizer=tf.keras.optimizers.Adam())
+
+        # train using cooling/heating schedule for lr/batch-size
+        for i in range(len(lr)):
+            
+            if verbose is True:
+                print('learning rate = ' + str(lr[i]) + ', batch size = ' + str(batch_size[i]))
+
+            # set learning rate
+            photulator.optimizer.lr = lr[i]
+
+            # split into validation and training sub-sets
+            n_validation = int(training_theta.shape[0] * validation_split)
+            n_training = training_theta.shape[0] - n_validation
+            training_selection = tf.random.shuffle([True] * n_training + [False] * n_validation)
+
+            # create iterable dataset (given batch size)
+            train_mag = tf.expand_dims(training_mag[:,f],-1)
+            training_data = tf.data.Dataset.from_tensor_slices((training_theta[training_selection], train_mag[training_selection])).shuffle(n_training).batch(batch_size[i])
+
+            # set up training loss
+            training_loss = [np.infty]
+            validation_loss = [np.infty]
+            best_loss = np.infty
+            early_stopping_counter = 0
+
+            # loop over epochs
+            while early_stopping_counter < patience:
+
+                # loop over batches for a single epoch
+                for theta, mag in training_data:
+
+                    # training step: check whether to accumulate gradients or not (only worth doing this for very large batch sizes)
+                    if gradient_accumulation_steps[i] == 1:
+                        loss = photulator.training_step(theta, mag)
+                    else:
+                        loss = photulator.training_step_with_accumulated_gradients(theta, mag, accumulation_steps=gradient_accumulation_steps[i])
+
+                # compute total loss and validation loss
+                validation_loss.append(photulator.compute_loss(training_theta[~training_selection], train_mag[~training_selection]).numpy())
+
+                # early stopping condition
+                if validation_loss[-1] < best_loss:
+                    best_loss = validation_loss[-1]
+                    early_stopping_counter = 0
+                else:
+                    early_stopping_counter += 1
+                if early_stopping_counter >= patience:
+                    photulator.update_emulator_parameters()
+                    photulator.save(root_dir + 'model_{}x{}'.format(n_layers, n_units) + filters[f])
+                    if verbose is True:
+                        print('Validation loss = ' + str(best_loss))
