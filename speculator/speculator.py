a71ef71ddc81f6355865a064e0af3f5bc0d2e49c
stevet40 <stephen.thorp@hotmail.co.uk>
2026-04-28 11:29:34 +0100

    Giving people the option of being safe

--- speculator/speculator.py
+++ speculator/speculator.py
@@ -32,6 +32,7 @@ class Speculator(torch.nn.Module):
         optimizer=lambda x: torch.optim.Adam(x, lr=1e-3),
         restore=False,
         restore_filename=None,
+        restore_weights_only=False,
         device="cpu",
     ):
         """
@@ -147,7 +148,7 @@ class Speculator(torch.nn.Module):
         self.optimizer = self.optimizer_constructor(self.params)
 
         if restore:
-            self.load_state_dict(torch.load(restore_filename, map_location=device, weights_only=False))
+            self.load_state_dict(torch.load(restore_filename, map_location=device, weights_only=restore_weights_only))
 
     # change the device we're on
     def set_device(self, device):
@@ -1040,13 +1041,13 @@ class Photulator(torch.nn.Module):
 
 class PhotulatorModelStack:
 
-    def __init__(self, root_dir, filenames, device="cpu"):
+    def __init__(self, root_dir, filenames, device="cpu", weights_only=False):
 
         # how many emulators?
         self.n_emulators = len(filenames)
 
         # load emulator models
-        self.emulators = [torch.load(filename, weights_only=False).to(device) for filename in filenames]
+        self.emulators = [torch.load(filename, weights_only=weights_only).to(device) for filename in filenames]
 
     # compute fluxes (in units of nano maggies) given SPS parameters (theta) and normalization (N = -2.5log10M + dm(z))
     def fluxes(self, theta, N):
